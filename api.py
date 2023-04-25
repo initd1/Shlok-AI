deaa72680b3697033ff1ebc5a5edc8b93ad23ad4
kash <kashvijayperth@gmail.com>
2023-04-26 01:28:13 +0800

    created api endpoint for shlok-ai web to call

---
+++ api.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify, request
+from . import shlok_ai_web
+
+app = Flask(__name__)
+
+@app.route('/api/shloka-prompt', methods=['POST'])
+def shloka_sage_ai():
+    prompt = request.form['prompt']
+    result = shlok_ai_web.shlokAI(prompt)
+    return jsonify(result=result)
+
+if __name__ == '__main__':
+    app.run()
